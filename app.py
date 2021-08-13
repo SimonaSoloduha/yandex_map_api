77b04be588a3388f5c12a718663a70b06c3eb1ce
Soloduha Simona <ls460simona@gmail.com>
2021-08-13 14:57:38 +0300

    add correct

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from search import search_all_kv_vt
+from search import search_all_kv_vt, write_all_kv_vr_to_json
 
 app = Flask(__name__)
 IP = '319343c0-2efc-4ada-b206-7e98ff41dd8e'
@@ -7,11 +7,9 @@ IP = '319343c0-2efc-4ada-b206-7e98ff41dd8e'
 
 @app.route('/')
 def index():
-    # data_kv = search_all_kv_vt()
-    # print(data_kv)
-    # write_all_kv_vr_to_json()
+    search_all_kv_vt()
+    write_all_kv_vr_to_json()
     return render_template('index.html')
-    # return render_template('index.html')
 
 
 @app.route('/map')
