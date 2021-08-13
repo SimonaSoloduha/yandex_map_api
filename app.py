38241c1effc2ce1e4aea1d7e23c8fb5ab4466e7f
Simona Soloduha <56197970+SimonaSoloduha@users.noreply.github.com>
2021-08-13 15:00:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,5 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/map')
-def map_kv():
-    return render_template('map.html')
-
-
 if __name__ == "__main__":
     app.run(debug=True)
